54bb360d729069a0cbb3b7595c3ee1530a0861fb
selloriwoo <sellossier@gmail.com>
2024-12-31 15:52:19 +0900

    first git

---
+++ src/main/java/com/umc7/ZIC/ZicApplication.java
@@ -0,0 +1,13 @@
+package com.umc7.ZIC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc7/ZIC/ZicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc7.ZIC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
