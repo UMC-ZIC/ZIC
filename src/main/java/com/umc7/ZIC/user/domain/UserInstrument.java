796d8306b4a6bf60642df8a7e3ff5c6690645316
selloriwoo <sellossier@gmail.com>
2025-01-03 17:31:40 +0900

    feature: 지역, 악기 entity 추가 및 유저_악기 매핑 엔티디, 유저와 지역 매핑 추가

---
+++ src/main/java/com/umc7/ZIC/user/domain/UserInstrument.java
@@ -0,0 +1,28 @@
+package com.umc7.ZIC.user.domain;
+
+import com.umc7.ZIC.common.domain.BaseEntity;
+import com.umc7.ZIC.common.domain.Instrument;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserInstrument extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "instrument_id")
+    private Instrument instrument;
+}
