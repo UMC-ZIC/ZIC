4051360bce3e226d3c8f5147a6556191d66beee1
selloriwoo <sellossier@gmail.com>
2025-01-03 17:54:48 +0900

    refactor: Region 엔티티 클래스 위치 수정 및 enum 값 대문자로 변경

--- src/main/java/com/umc7/ZIC/common/domain/enums/InstrumentType.java
+++ src/main/java/com/umc7/ZIC/common/domain/enums/InstrumentType.java
@@ -4,16 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum InstrumentType {
-    piano("피아노"),
-    violin("바이올린"),
-    guitar("기타"),
-    bass("베이스"),
-    drum("드럼"),
-    flute("플롯"),
-    harp("하프"),
-    trumpet("트럼펫"),
-    viola("비올라"),
-    samulnori("사물놀이");
+    PIANO("피아노"),
+    VIOLIN("바이올린"),
+    GUITAR("기타"),
+    BASS("베이스"),
+    DRUM("드럼"),
+    FLUTE("플롯"),
+    HARP("하프"),
+    TRUMPET("트럼펫"),
+    VIOLA("비올라"),
+    SAMULNORI("사물놀이");
 
     private final String koreanName;
 
