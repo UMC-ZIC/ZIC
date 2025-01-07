110be03033e48aa72f904704984cfbba9885aa86
sellen <austin0801@naver.com>
2025-01-07 17:24:45 +0900

    refactor: DTO 타입을 class에서 record로 변경

--- src/main/java/com/umc7/ZIC/apiPayload/code/ErrorReasonDTO.java
+++ src/main/java/com/umc7/ZIC/apiPayload/code/ErrorReasonDTO.java
@@ -4,15 +4,14 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
 @Builder
-public class ErrorReasonDTO {
-
-    private HttpStatus httpStatus;
-
-    private final boolean isSuccess;
-    private final String code;
-    private final String message;
-
-    public boolean getIsSuccess(){return isSuccess;}
+public record ErrorReasonDTO(
+        HttpStatus httpStatus,
+        boolean isSuccess,
+        String code,
+        String message
+) {
+    public boolean getIsSuccess() {
+        return isSuccess;
+    }
 }
--- src/main/java/com/umc7/ZIC/apiPayload/code/ReasonDTO.java
+++ src/main/java/com/umc7/ZIC/apiPayload/code/ReasonDTO.java
@@ -4,15 +4,14 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
 @Builder
-public class ReasonDTO {
-
-    private HttpStatus httpStatus;
-
-    private final boolean isSuccess;
-    private final String code;
-    private final String message;
-
-    public boolean getIsSuccess(){return isSuccess;}
+public record ReasonDTO (
+        HttpStatus httpStatus,
+        boolean isSuccess,
+        String code,
+        String message
+) {
+    public boolean getIsSuccess() {
+        return isSuccess;
+    }
 }
