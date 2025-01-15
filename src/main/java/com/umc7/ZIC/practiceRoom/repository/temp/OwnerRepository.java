470a6bd8a1fbf13a81523869794a549d456eaabd
selloriwoo <sellossier@gmail.com>
2025-01-15 15:47:49 +0900

    remove: 테스트용 repository 삭제

--- src/main/java/com/umc7/ZIC/practiceRoom/repository/temp/OwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.umc7.ZIC.practiceRoom.repository.temp;
-
-import com.umc7.ZIC.owner.domain.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwnerRepository extends JpaRepository<Owner, Long> {
-}
