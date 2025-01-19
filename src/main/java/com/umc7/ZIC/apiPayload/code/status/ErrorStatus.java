0ab4576c2b5375d42600c007fdbb9863feaa8705
DongWooKim4343 <kd6904@naver.com>
2025-01-19 22:30:52 +0900

    feature: 연습실 예외 코드 추가

--- src/main/java/com/umc7/ZIC/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/umc7/ZIC/apiPayload/code/status/ErrorStatus.java
@@ -29,7 +29,10 @@ public enum ErrorStatus implements BaseErrorCode {
     JWT_EMPTY(HttpStatus.UNAUTHORIZED, "JWT4003", "JWT 토큰을 넣어주세요."),
     JWT_EXPIRED(HttpStatus.UNAUTHORIZED, "JWT4004", "만료된 토큰입니다."),
     JWT_REFRESHTOKEN_NOT_MATCHED(HttpStatus.UNAUTHORIZED, "JWT4005", "RefreshToken이 일치하지 않습니다."),
-    ;
+
+    //연습실 관련 에러
+    PRACTICEROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "PRACTICEROOM4004", "연습실이 없습니다."),
+    PRACTICEROOM_AUTHORIZATION_FAILED(HttpStatus.UNAUTHORIZED, "JPRACTICEROOM4001", "해당 연습실에 권한이 없습니다.");
 
 
     private final HttpStatus httpStatus;
