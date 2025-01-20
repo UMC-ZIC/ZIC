a9d0b98441697eef4bf29f8b343b66913ac3a045
DongWooKim4343 <kd6904@naver.com>
2025-01-21 02:45:39 +0900

    feature: 연습실 내부 방 DTO 구현

---
+++ src/main/java/com/umc7/ZIC/practiceRoom/dto/PracticeRoomDetailRequestDto.java
@@ -0,0 +1,40 @@
+package com.umc7.ZIC.practiceRoom.dto;
+
+import com.umc7.ZIC.practiceRoom.domain.PracticeRoom;
+import com.umc7.ZIC.practiceRoom.domain.PracticeRoomDetail;
+import com.umc7.ZIC.practiceRoom.domain.enums.RoomStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record PracticeRoomDetailRequestDto() {
+    public record CreateRequestDetailDto(
+            @NotBlank(message = "연습방 이름은 필수입니다.")
+            String name,
+            String image,
+            @NotNull(message = "가격은 필수입니다.")
+            @PositiveOrZero(message = "가격은 0 이상이어야 합니다.")
+            Integer fee,
+            @NotNull(message = "이용 상태는 필수입니다.")
+            RoomStatus status
+    ) {
+        public PracticeRoomDetail toEntity(PracticeRoom practiceRoom) {
+            return PracticeRoomDetail.builder()
+                    .practiceRoom(practiceRoom)
+                    .name(this.name)
+                    .image(this.image)
+                    .fee(this.fee)
+                    .status(this.status)
+                    .build();
+        }
+    }
+
+    public record UpdateRequestDetailDto(
+            String name,
+            String image,
+            @PositiveOrZero(message = "가격은 0 이상이어야 합니다.")
+            Integer fee,
+            RoomStatus status
+    ) {
+    }
+}
