928df5ebcfacba0c7dd6c8964998c6771fee07b7
DongWooKim4343 <kd6904@naver.com>
2025-01-27 00:09:16 +0900

    feature: 연습실 좋아요 관련 DTO

---
+++ src/main/java/com/umc7/ZIC/practiceRoom/dto/PracticeRoomLikeRequestDto.java
@@ -0,0 +1,11 @@
+package com.umc7.ZIC.practiceRoom.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public record PracticeRoomLikeRequestDto() {
+
+    public record LikeRequestDto(
+
+    ) {}
+}
+
