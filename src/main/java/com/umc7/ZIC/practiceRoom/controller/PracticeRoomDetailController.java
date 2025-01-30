e34cacadbb6a861af9167159220b278346c9b938
DongWooKim4343 <kd6904@naver.com>
2025-01-31 03:07:24 +0900

    feature: 연습실 내부 방 등록 시 JWT 토큰 방식 수정

--- src/main/java/com/umc7/ZIC/practiceRoom/controller/PracticeRoomDetailController.java
+++ src/main/java/com/umc7/ZIC/practiceRoom/controller/PracticeRoomDetailController.java
@@ -1,10 +1,13 @@
 package com.umc7.ZIC.practiceRoom.controller;
 
+import com.umc7.ZIC.apiPayload.code.status.ErrorStatus;
 import com.umc7.ZIC.apiPayload.exception.ApiResponse;
+import com.umc7.ZIC.apiPayload.exception.handler.UserHandler;
 import com.umc7.ZIC.practiceRoom.dto.PageRequestDto;
 import com.umc7.ZIC.practiceRoom.dto.PracticeRoomDetailRequestDto;
 import com.umc7.ZIC.practiceRoom.dto.PracticeRoomDetailResponseDto;
 import com.umc7.ZIC.practiceRoom.service.PracticeRoomDetailService;
+import com.umc7.ZIC.security.JwtTokenProvider;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -19,14 +22,20 @@ import org.springframework.web.bind.annotation.*;
 public class PracticeRoomDetailController {
 
     private final PracticeRoomDetailService practiceRoomDetailService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     //연습실 내부 방 등록
     @PostMapping("/rooms")
     @Operation(summary = "연습실 내부 방 등록 API", description = "연습실에 연습방을 등록할때 사용하는 API.")
     public ApiResponse<PracticeRoomDetailResponseDto.CreateResponseDto> createPracticeRoomDetail(
             @RequestBody @Valid PracticeRoomDetailRequestDto.CreateRequestDetailDto createRequest,
-            @PathVariable Long practiceRoomId,
-            @RequestParam Long userId) {
+            @PathVariable Long practiceRoomId) {
+
+        if (jwtTokenProvider.resolveAccessToken().isEmpty()) {
+            throw new UserHandler(ErrorStatus._UNAUTHORIZED);
+        }
+
+        Long userId = jwtTokenProvider.getUserIdFromToken();
         PracticeRoomDetailResponseDto.CreateResponseDto response = practiceRoomDetailService.createPracticeRoomDetail(createRequest, practiceRoomId, userId);
         return ApiResponse.onSuccess(response);
     }
@@ -42,33 +51,43 @@ public class PracticeRoomDetailController {
     }
 
     //연습실 내부 방 단일 조회
-    @GetMapping("/rooms/{room_id}")
+    @GetMapping("/rooms/{roomId}")
     @Operation(summary = "연습실 내부 방 단일 조회 API", description = "연습실 내부 방을 단일 조회하는 API.")
     public ApiResponse<PracticeRoomDetailResponseDto.GetResponseDto> getPracticeRoomDetail(
-            @PathVariable Long room_id,
+            @PathVariable Long roomId,
             @PathVariable Long practiceRoomId) {
-        PracticeRoomDetailResponseDto.GetResponseDto response = practiceRoomDetailService.getPracticeRoomDetail(room_id, practiceRoomId);
+        PracticeRoomDetailResponseDto.GetResponseDto response = practiceRoomDetailService.getPracticeRoomDetail(roomId, practiceRoomId);
         return ApiResponse.onSuccess(response);
     }
 
     //연습실 내부 방 정보 수정
-    @PatchMapping("/rooms/{room_id}")
+    @PatchMapping("/rooms/{roomId}")
     @Operation(summary = "연습실 내부 방 정보 수정 API", description = "연습실 내부 방 정보를 수정하는 API.")
     public ApiResponse<PracticeRoomDetailResponseDto.UpdateResponseDto> updatePracticeRoomDetail(
             @RequestBody @Valid PracticeRoomDetailRequestDto.UpdateRequestDetailDto updateRequest,
             @PathVariable Long practiceRoomId,
-            @PathVariable Long room_id) {
-        PracticeRoomDetailResponseDto.UpdateResponseDto response = practiceRoomDetailService.updatePracticeRoomDetail(updateRequest, practiceRoomId, room_id);
+            @PathVariable Long roomId) {
+        if (jwtTokenProvider.resolveAccessToken().isEmpty()) {
+            throw new UserHandler(ErrorStatus._UNAUTHORIZED);
+        }
+
+        Long userId = jwtTokenProvider.getUserIdFromToken();
+        PracticeRoomDetailResponseDto.UpdateResponseDto response = practiceRoomDetailService.updatePracticeRoomDetail(updateRequest, practiceRoomId, roomId, userId);
         return ApiResponse.onSuccess(response);
     }
 
     //연습실 내부 방 삭제
-    @DeleteMapping("/rooms/{room_id}")
+    @DeleteMapping("/rooms/{roomId}")
     @Operation(summary = "연습실 내부 방 삭제 API", description = "연습실 내부 방을 삭제하는 API.")
     public ApiResponse<Void> deletePracticeRoomDetail(
             @PathVariable Long practiceRoomId,
-            @PathVariable Long room_id) {
-        practiceRoomDetailService.deletePracticeRoomDetail(practiceRoomId, room_id);
+            @PathVariable Long roomId) {
+        if (jwtTokenProvider.resolveAccessToken().isEmpty()) {
+            throw new UserHandler(ErrorStatus._UNAUTHORIZED);
+        }
+
+        Long userId = jwtTokenProvider.getUserIdFromToken();
+        practiceRoomDetailService.deletePracticeRoomDetail(practiceRoomId, roomId, userId);
         return ApiResponse.onSuccess(null);
     }
 }
