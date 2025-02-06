cba5bca546d8ce79577c117edc56a1a1228308b5
DongWooKim4343 <kd6904@naver.com>
2025-02-06 22:54:34 +0900

    refactor: 필드명 및 타입변경

--- src/main/java/com/umc7/ZIC/practiceRoom/dto/PageResponseDto.java
+++ src/main/java/com/umc7/ZIC/practiceRoom/dto/PageResponseDto.java
@@ -2,19 +2,25 @@ package com.umc7.ZIC.practiceRoom.dto;
 
 import org.springframework.data.domain.Page;
 
-public record PageResponseDto(
-        Integer totalPages, // 전체 페이지 수
-        Integer currentPage, // 현재 페이지 번호 (1부터 시작)
-        Long totalElements, // 전체 요소 개수
-        Integer currentSize // 현재 페이지의 요소 개수
+import java.util.List;
+
+public record PageResponseDto<T>(
+        List<T> resultList,   // 실제 데이터 목록
+        Integer listSize,    // 현재 페이지의 데이터 개수
+        Integer totalPage,  // 전체 페이지 수
+        Long totalElements,  // 전체 요소 개수 (모든페이지를 합한수)
+        Boolean isFirst,      // 현재 페이지가 첫 페이지인지 여부
+        Boolean isLast       // 현재 페이지가 마지막 페이지인지 여부
 ) {
 
-    public static PageResponseDto from(Page<?> page) {
-        return new PageResponseDto(
-                page.getTotalPages(),
-                page.getNumber() + 1, // 0-based index이므로 1을 더함
-                page.getTotalElements(),
-                page.getNumberOfElements()
+    public static <T> PageResponseDto<T> from(Page<T> page) {
+        return new PageResponseDto<>(
+                page.getContent(),      // 데이터 목록
+                page.getNumberOfElements(), // 현재 페이지의 요소 개수
+                page.getTotalPages(),       // 전체 페이지 수
+                page.getTotalElements(),    // 전체 요소 개수
+                page.isFirst(),         // 현재 페이지가 첫 페이지인지 여부
+                page.isLast()          // 현재 페이지가 마지막 페이지인지 여부
         );
     }
 }
