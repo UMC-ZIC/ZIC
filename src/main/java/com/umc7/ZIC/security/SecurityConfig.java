f2a450b64da5da6df42ff4a2c8bb2a2984eca479
selloriwoo <sellossier@gmail.com>
2025-02-07 17:52:09 +0900

    feature: cors 테스트위한 코드

--- src/main/java/com/umc7/ZIC/security/SecurityConfig.java
+++ src/main/java/com/umc7/ZIC/security/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -55,6 +60,31 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*"); // 모든 출처 허용 (필요시 특정 도메인으로 변경)
+        // 허용할 Origin 도메인 설정
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://localhost:3000",
+                "http://localhost:5173",
+                "https://localhost:5173",
+                "http://localhost:8080",
+                "https://localhost:8080",
+                "http://43.200.3.214:8080",
+                "https://43.200.3.214:8080",
+                "http://zic-eight.vercel.app/",
+                "https://zic-eight.vercel.app"
+        ));
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true); // 자격 증명 허용 (쿠키, 인증 정보 포함)
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
