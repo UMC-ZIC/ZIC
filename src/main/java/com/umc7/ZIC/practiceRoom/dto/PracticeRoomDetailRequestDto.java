cad5701073ed7363d57baa9723aa6662ca73db65
DongWooKim4343 <kd6904@naver.com>
2025-02-11 17:29:08 +0900

    feature: 유효성 검사 추가

--- src/main/java/com/umc7/ZIC/practiceRoom/dto/PracticeRoomDetailRequestDto.java
+++ src/main/java/com/umc7/ZIC/practiceRoom/dto/PracticeRoomDetailRequestDto.java
@@ -6,14 +6,17 @@ import com.umc7.ZIC.practiceRoom.domain.enums.RoomStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 
 public record PracticeRoomDetailRequestDto() {
     public record CreateRequestDetailDto(
             @NotBlank(message = "연습방 이름은 필수입니다.")
+            @Size(max = 20, message = "연습방 이름은 20자 이하여야 합니다.")
             String name,
             String image,
             @NotNull(message = "가격은 필수입니다.")
             @PositiveOrZero(message = "가격은 0 이상이어야 합니다.")
+            @Size(max = 20, message = "가격은 20자 이하여야 합니다.")
             Integer fee,
             @NotNull(message = "이용 상태는 필수입니다.")
             RoomStatus status
