ab8cf090bdee1c3606b9032bfe814918925035c9
selloriwoo <sellossier@gmail.com>
2025-02-14 17:06:31 +0900

    feature: redis 구현

---
+++ src/main/java/com/umc7/ZIC/common/util/RedisUtil.java
@@ -0,0 +1,42 @@
+package com.umc7.ZIC.common.util;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Set;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RedisUtil {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public void setDataAndExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public String getValue(String key) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public void deleteData(String key) {
+        stringRedisTemplate.delete(key);
+    }
+    
+    //키 패턴으로 찾기
+    public Set<String> getKeys(String keyPattern){return stringRedisTemplate.keys(keyPattern);}
+
+
+    public void deleteAllData(Set<String> keys) {stringRedisTemplate.delete(keys);}
+
+    public void setDataExpire(String email, String code, long l) {
+    }
+}
