3986f5f8216aeff898b0cfd7d5fdb77dee18c8a0
DongWooKim4343 <kd6904@naver.com>
2025-02-19 16:57:19 +0900

    style: 불필요 임포트 제거

--- src/main/java/com/umc7/ZIC/practiceRoom/controller/PracticeRoomController.java
+++ src/main/java/com/umc7/ZIC/practiceRoom/controller/PracticeRoomController.java
@@ -20,7 +20,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
