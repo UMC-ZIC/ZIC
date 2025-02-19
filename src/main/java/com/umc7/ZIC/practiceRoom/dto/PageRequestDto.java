8a06c7a96882d1cb6c60aab3123a10f33ca5b778
DongWooKim4343 <kd6904@naver.com>
2025-02-19 16:53:28 +0900

    fix: 스웨거에서 body 형식으로 요청되는 부분 파라미터로 수정 및 오름차순 정렬

--- src/main/java/com/umc7/ZIC/practiceRoom/dto/PageRequestDto.java
+++ src/main/java/com/umc7/ZIC/practiceRoom/dto/PageRequestDto.java
@@ -1,13 +1,19 @@
 package com.umc7.ZIC.practiceRoom.dto;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-
+@ParameterObject
 public record PageRequestDto(
+        @Parameter(description = "페이지 번호 (1 이상)")
         @Min(value = 1, message = "페이지 번호는 1 이상이어야 합니다.")
         int page,
+
+        @Parameter(description = "페이지 크기 (10-100 사이)")
         @Min(value = 10)
         @Max(value = 100)
         int size
@@ -19,6 +25,6 @@ public record PageRequestDto(
     }
 
     public Pageable toPageable() {
-        return org.springframework.data.domain.PageRequest.of(page - 1, size, Sort.by("id").descending());
+        return org.springframework.data.domain.PageRequest.of(page - 1, size, Sort.by("id").ascending());
     }
 }
