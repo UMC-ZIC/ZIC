8ffd81045c32659e49b2e62577271ccea1fb6fb4
DongWooKim4343 <kd6904@naver.com>
2025-02-19 16:59:28 +0900

    feature: 연습실 내부방 목록조회 반환데이터에 이용가능시간 추가

--- src/main/java/com/umc7/ZIC/practiceRoom/service/PracticeRoomDetailService.java
+++ src/main/java/com/umc7/ZIC/practiceRoom/service/PracticeRoomDetailService.java
@@ -1,5 +1,6 @@
 package com.umc7.ZIC.practiceRoom.service;
 
+import com.umc7.ZIC.practiceRoom.domain.PracticeRoomDetail;
 import com.umc7.ZIC.practiceRoom.dto.*;
 
 import java.time.LocalDate;
@@ -8,7 +9,7 @@ import java.util.List;
 
 public interface PracticeRoomDetailService {
     PracticeRoomDetailResponseDto.CreateDetailResponseDto createPracticeRoomDetail(PracticeRoomDetailRequestDto.CreateRequestDetailDto createRequest, Long practiceRoomId, Long userId);
-    PageResponseDto<PracticeRoomDetailResponseDto.GetDetailResponseDto> getPracticeRoomDetailList(PageRequestDto request, Long practiceRoomId);
+    PageResponseDto<PracticeRoomDetailResponseDto.GetListDetailResponseDto> getPracticeRoomDetailList(PageRequestDto request, Long practiceRoomId, LocalDate date);
     PracticeRoomDetailResponseDto.GetDetailResponseDto getPracticeRoomDetail(Long practiceRoomDetailId);
     PracticeRoomDetailResponseDto.UpdateDetailResponseDto updatePracticeRoomDetail(PracticeRoomDetailRequestDto.UpdateRequestDetailDto updateRequest, Long practiceRoomDetailId, Long userId);
     void deletePracticeRoomDetail(Long practiceRoomDetailId, Long userId);
@@ -17,4 +18,6 @@ public interface PracticeRoomDetailService {
 
     // 대여자용 연습실 내부방 리스트 조회
     PracticeRoomDetailResponseDto.GetOwnerDetailResponseDto getOwnerPracticeRoomDetailList(Long userId);
+
+    boolean hasAvailableTimeSlot(PracticeRoomDetail practiceRoomDetail, LocalDate date);
 }
